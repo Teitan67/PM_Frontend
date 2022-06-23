5a817b5382f7f3bc8a3b35a17577daefb49e4a89
Oscar Roberto Velásquez León <Oskrr67@gmail.com>
2022-06-23 09:56:52 -0600

    Preparando cambios antes del merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
 import "../css/footer-style.css"
 
-export default class Footer extends Component{
+export default class Footer extends Component {
 
-    ActualYear(){
-        const time=new Date()
-        var year=time.getFullYear()
+    ActualYear() {
+        const time = new Date()
+        var year = time.getFullYear()
         return year.toString()
     }
 
 
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
-                    <div className='GenFooter '>
-                    <div className='container-fluid justify-content-center text-center bg-dark text-white pt-4'>
-                       
-                            <div className='logoFooter pt-2 pb-2'>
-                            <img src='/assets/logoM.png' alt='MayalandSolutions'/>
-                            
-                            <p>Copyright &#169; {this.ActualYear()} Mayaland Solutions.<br/> All Rights Reserved.</p>      
+                <div className='GenFooter '>
+                    <div className='container-fluid justify-content-center text-center bg-dark text-white pt-4 GenFooter'>
+
+                        <div className='logoFooter pt-2 pb-2'>
+                            <img src='/assets/logoM.png' alt='MayalandSolutions' />
+
+                            <p>Copyright &#169; {this.ActualYear()} Mayaland Solutions.<br /> All Rights Reserved.</p>
                         </div>
-                        
+
                     </div>
-                    </div>
-                    
-                
+                </div>
             </React.Fragment>
         )
     }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,119 +1,120 @@
 import React, { Component } from 'react'
 import "../css/login-style.css"
 import Footer from '../components/Footer'
-import { AiOutlineUser,AiOutlineKey,AiOutlineLogin } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineKey, AiOutlineLogin } from 'react-icons/ai';
 import { selectWithDataService } from '../services/auth/authservices';
 import { setNewCookie } from '../services/cookieService';
-import {automaticCloseAlert} from'../functions/alerts'
+import { automaticCloseAlert } from '../functions/alerts'
 
 import md5 from 'md5'
 
-import {closeSession} from "../functions/closeSession"
+import { closeSession } from "../functions/closeSession"
 
 
 
 
 
 export default class Login extends Component {
-   
-  
 
-  state={
-    loginStorage:{
-      userName:'',
-      password:''
+
+
+  state = {
+    loginStorage: {
+      userName: '',
+      password: ''
     }
   }
 
-  onChangeById=e=>{
-    var idState=e.target.id.split('_')
-    const temp=this.state.loginStorage
-    if(idState.length===2){
-      temp[idState[1]]=e.target.value
+  onChangeById = e => {
+    var idState = e.target.id.split('_')
+    const temp = this.state.loginStorage
+    if (idState.length === 2) {
+      temp[idState[1]] = e.target.value
     }
 
-    this.setState({loginStorage:temp})
+    this.setState({ loginStorage: temp })
   }
 
-  onKeyDown=async e=>{
-    if(e.key==='Enter'){
+  onKeyDown = async e => {
+    if (e.key === 'Enter') {
       await this.Login()
     }
   }
 
-    
 
-  async Login(){
-    const temp=this.state.loginStorage
 
-    const datos={
+  async Login() {
+    const temp = this.state.loginStorage
+
+    const datos = {
       userName: temp.userName,
       password: md5(temp.password),
     }
-    const datos2=await selectWithDataService(datos,"/login")
-    if(datos2!=null){
-    if(datos2.status.code===1){
-      if(datos2.token!=='invalid'){
-        await automaticCloseAlert('correct','Welcome: '+datos2.data.name+" "+datos2.data.surname)
-        await setNewCookie('sessionAuthToken',datos2.token,50)
-        await setNewCookie('userName',datos2.data.userName,50)
-        await setNewCookie('name',datos2.data.name,50)
-        await setNewCookie('surname',datos2.data.surname,50)
-        window.location.href='/companyDashBoard'
-      }else{
-        closeSession()
-        automaticCloseAlert('incorrect','Your  Username or Password are incorrect. Please try again')
+    const datos2 = await selectWithDataService(datos, "/login")
+    if (datos2 != null) {
+      if (datos2.status.code === 1) {
+        if (datos2.token !== 'invalid') {
+          await automaticCloseAlert('correct', 'Welcome: ' + datos2.data.name + " " + datos2.data.surname)
+          await setNewCookie('sessionAuthToken', datos2.token, 50)
+          await setNewCookie('userName', datos2.data.userName, 50)
+          await setNewCookie('name', datos2.data.name, 50)
+          await setNewCookie('surname', datos2.data.surname, 50)
+          window.location.href = '/companyDashBoard'
+        } else {
+          closeSession()
+          automaticCloseAlert('incorrect', 'Your  Username or Password are incorrect. Please try again')
+        }
       }
     }
-  }
   }
 
 
 
   render() {
-   
+
     return (
-      <div className='login'>
-        <div className='container-fluid pb-1'>
-        <div className='container'>
-          <div className='row pt-4 pb-3 d-flex justify-content-center'>
-            <div className='col-9 contLogin'>
-              <div className='row d-flex justify-content-center pt-5'>
-                <div className='userImg'>
-                  <img src='/assets/userico.png' alt='' />
+      <div className='login  '>
+        <div className='container-fluid pb-5 '>
+          <div className='container '>
+            <div className='row pt-4 pb-3 d-flex justify-content-center'>
+
+              <div className='col-9 contLogin'>
+                <div class="border border-5 border-primary"></div>
+                <div className='row d-flex justify-content-center pt-5   '>
+                  <p className='display-2 '>Login</p>
+
                 </div>
 
-              </div>
-              <div className='row d-flex justify-content-center pt-5 pb-3'>
-                <div className='col-11'>
-                  <div className="input-group input-group-lg">
-                    <span className="input-group-text"><AiOutlineUser/></span>
-                    <input type="text" id='Login_userName' onChange={this.onChangeById} onKeyDown={this.onKeyDown} className="form-control" placeholder='Username'/>
+                <div className='row d-flex justify-content-center pt-5 pb-3'>
+                  <div className='col-11'>
+                    <div className="input-group input-group-lg">
+                      <span className="input-group-text"><AiOutlineUser /></span>
+                      <input type="text" id='Login_userName' onChange={this.onChangeById} onKeyDown={this.onKeyDown} className="form-control" placeholder='Username' />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className='row d-flex justify-content-center pt-4 pb-3'>
-                <div className='col-11'>
-                  <div className="input-group input-group-lg">
-                    <span className="input-group-text"><AiOutlineKey/></span>
-                    <input type="password"  id='Login_password' onKeyDown={this.onKeyDown} onChange={this.onChangeById} className="form-control" placeholder='Password'/>
+                <div className='row d-flex justify-content-center pt-4 pb-3'>
+                  <div className='col-11'>
+                    <div className="input-group input-group-lg">
+                      <span className="input-group-text"><AiOutlineKey /></span>
+                      <input type="password" id='Login_password' onKeyDown={this.onKeyDown} onChange={this.onChangeById} className="form-control" placeholder='Password' />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className='row d-flex justify-content-center pt-4 pb-3 text-center'>
-                <div className='col-7'>
-                  <button type="button" className="btn btn-primary btn-lg" onClick={()=>this.Login()}>Login <AiOutlineLogin/></button>
+                <div className='row d-flex justify-content-center pt-4 pb-3 text-center'>
+                  <div className='col-7'>
+                    <button type="button" className="btn btn-primary btn-lg" onClick={() => this.Login()}>sign in<AiOutlineLogin /></button>
+                  </div>
                 </div>
-              </div>
 
+              </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer/>
-      </div>
-      
+
 
 
     )

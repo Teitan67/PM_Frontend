41196c43da7dc123b302a05221716ddcd9cf89ba
DiegoPabloPA <2963890720101@ingenieria.usac.edu.gt>
2022-07-13 21:44:24 -0600

    Updates

---
+++ precache-manifest.de276f10abd8067231e726171b6b1896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4877b929de9315f641b4",
+    "url": "./static/css/main.8d62de81.chunk.css"
+  },
+  {
+    "revision": "4877b929de9315f641b4",
+    "url": "./static/js/main.4877b929.chunk.js"
+  },
+  {
+    "revision": "e4544c4f6e8780f2a99b",
+    "url": "./static/css/1.2e7dbd24.chunk.css"
+  },
+  {
+    "revision": "e4544c4f6e8780f2a99b",
+    "url": "./static/js/1.e4544c4f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "96c8bd2ee87c862085d08a4cc76c6909",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.91d61e0fe02a326b85e204c65adca843.js"
+  "./precache-manifest.de276f10abd8067231e726171b6b1896.js"
 );
 
 workbox.clientsClaim();
